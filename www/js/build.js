703013db29a4caa7aabcc3832f42db50c2222707
Chase Wilson <chase@agroism.com>
2012-12-06 01:35:04 -0800

    Updated the build process for blocks, so now it includes the full versions of yate && yeah.

--- www/js/build.js
+++ www/js/build.js
@@ -5,19 +5,8 @@
     ,{name:'yaul',location:'yaul/src'}
     ,{name:'yeah',location:'yeah/src',main:'index'}
     ,{name:'yate',location:'yate/src',main:'index'}
-
   ]
-  // ,modules: [
-  //   {
-  //      name:'blocks'
-  //     ,include: ['blocks/block']
-  //   }
-  // ]
-  ,name: 'blocks'
-  // ,paths:{
-  //   'blocks/block':'blocks/block/index'
-  // }
-  ,include: ['blocks/block']
+  ,include: ['blocks/block','yeah','yate']
   ,optimize: 'none'
   ,stubModule:['text']
   //,findNestedDependencies: true
